180bd4376d6cd300ac3e0b38cedb3df08ec54a6b
UmutAdaYuruten <umutada@gmail.com>
2022-04-10 13:43:06 +0300

    minor

--- code/api/urls.py
+++ code/api/urls.py
@@ -5,7 +5,7 @@ urlpatterns= [
     path('analysis_get_id/<int:pk>/', analyses_get_by_id),
     path('analysis_posts/', analyses_post),
     path('analysis_get/', get_all_samples),
-    path('login/',login),
+    path('login/<pk>',login),
     path('sign-up/',signup)
     #path('/analysis/:id',analyses),
     #path('/profile',profile),
