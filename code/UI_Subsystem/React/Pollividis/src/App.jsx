98a2ebfa0621098662a735246103da2a8c3eacef
iremtekin <iremtekin67@gmail.com>
2022-04-11 12:53:50 +0300

    minor

--- code/UI_Subsystem/React/Pollividis/src/App.jsx
+++ code/UI_Subsystem/React/Pollividis/src/App.jsx
@@ -1,11 +1,4 @@
 import { Grid, makeStyles } from "@material-ui/core";
-import Add from "./components/Add";
-import Feed from "./components/Feed";
-import Leftbar from "./components/Leftbar";
-import Navbar from "./components/Navbar";
-import Rightbar from "./components/Rightbar";
-import AnalyzeSample from "./components/AnalyzeSample";
-import SampleImagePreviewCard from "./components/SampleImagePreviewCard";
 import PollenMap from "./components/PollenMap";
 
 const useStyles = makeStyles((theme) => ({
