51375177f58ca4a50f1e2ed5bb817955e10cf33e
UmutAdaYuruten <umutada@gmail.com>
2022-05-03 19:44:42 +0300

    filter completed

--- code/api/urls.py
+++ code/api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import analyses_get_by_id, analyses_post, analyze, get_academic_by_id, get_all_samples, get_samples_of_academic, login, signup, add_feedback
+from .views import analyses_get_by_id, analyses_post, analyze, get_academic_by_id, get_all_samples, get_samples_of_academic, login, signup, add_feedback,get_samples_by_filter
 
 urlpatterns= [
     path('analysis_get_id/<int:pk>/', analyses_get_by_id),
@@ -10,7 +10,8 @@ urlpatterns= [
     path('feedback/', add_feedback),
     path('get_academic_by_id/<int:pk>/',get_academic_by_id),
     path('get_samples_of_academic/<int:pk>/',get_samples_of_academic),
-    path('analyze/',analyze)
+    path('analyze/',analyze),
+    path('get_filtered_samples/',get_samples_by_filter)
     #path('/analysis/:id',analyses),
     #path('/profile',profile),
     #path('/map',gmap)  
